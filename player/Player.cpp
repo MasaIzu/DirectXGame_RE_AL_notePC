09827f491176112f06597ee513a9666a31aafb56
LC1B_04_Izumida_Masato <k021g1329@g.neec.ac.jp>
2022-06-29 11:51:01 +0900

    プロジェクト ファイルを追加します。

---
+++ player/Player.cpp
@@ -0,0 +1,105 @@
+#include "Player.h"
+
+
+void Player::Initialize(Model* model, uint32_t textureHandle) {
+	//NULLポインタチェック
+	assert(model);
+	model_ = model;
+	textureHandle_ = textureHandle;
+
+	//シングルインスタンスを取得する
+	input_ = Input::GetInstance();
+	debugText_ = DebugText::GetInstance();
+
+	//ワールド変換の初期化
+	worldTransform_.Initialize();
+}
+
+void Player::Move() {
+
+	//キャラクターの移動ベクトル
+	Vector3 move = { 0, 0, 0 };
+	//キャラクターの移動の速さ
+	const float kCharacterSpeed = 0.2f;
+
+	//押した方向で移動ベクトルを変更
+	if (input_->PushKey(DIK_UP)) {
+		move = { 0, kCharacterSpeed, 0 };
+	}
+	else if (input_->PushKey(DIK_DOWN)) {
+		move = { 0, -kCharacterSpeed, 0 };
+	}
+	if (input_->PushKey(DIK_LEFT)) {
+		move = { -kCharacterSpeed, 0, 0 };
+	}
+	else if (input_->PushKey(DIK_RIGHT)) {
+		move = { kCharacterSpeed, 0, 0 };
+	}
+
+	worldTransform_.translation_ += move;
+
+	//行列更新
+	AffinTrans::affin(worldTransform_);
+
+	worldTransform_.TransferMatrix();
+
+	const float kMoveLimitX = 35;
+	const float kMoveLimitY = 18;
+
+	//範囲を超えない処理
+	worldTransform_.translation_.x = max(worldTransform_.translation_.x, -kMoveLimitX);
+	worldTransform_.translation_.x = min(worldTransform_.translation_.x, +kMoveLimitX);
+	worldTransform_.translation_.y = max(worldTransform_.translation_.y, -kMoveLimitY);
+	worldTransform_.translation_.y = min(worldTransform_.translation_.y, +kMoveLimitY);
+
+	const float kChestRotSpeed = 0.05f;
+
+	//押した方向で移動ベクトルを変更
+	if (input_->PushKey(DIK_U)) {
+		worldTransform_.rotation_.y -= kChestRotSpeed;
+	}
+	else if (input_->PushKey(DIK_I)) {
+		worldTransform_.rotation_.y += kChestRotSpeed;
+	}
+
+	//弾発射処理
+	Attack();
+
+	//弾更新
+	for (std::unique_ptr<PlayerBullet>& bullet : bullets_) {
+		bullet->Update();
+	}
+
+	debugText_->SetPos(50, 150);
+	debugText_->Printf(
+		"translation : %f,%f,%f", worldTransform_.translation_.x,
+		worldTransform_.translation_.y,
+		worldTransform_.translation_.z);
+
+}
+
+void Player::Update() {
+
+	Move();
+
+}
+
+void Player::Draw(ViewProjection& viewProjection_) {
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	//弾更新
+	for (std::unique_ptr<PlayerBullet>& bullet : bullets_) {
+		bullet->Draw(viewProjection_);
+	}
+}
+
+void Player::Attack() {
+	if (input_->PushKey(DIK_SPACE)) {
+		//弾を生成し初期化
+		std::unique_ptr<PlayerBullet> newBullet = std::make_unique<PlayerBullet>();
+		newBullet->Initialize(model_, worldTransform_.translation_);
+
+		//弾の登録
+		bullets_.push_back(std::move(newBullet));
+	}
+
+}
---
+++ player/Player.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "WorldTransform.h"
+#include "Model.h"
+#include <assert.h>
+#include "Input.h"
+#include "DebugText.h"
+#include "affin.h"
+#include "PlayerBullet.h"
+#include "memory"
+#include <list>
+
+
+class Player {
+
+public:
+	/// <summary>
+	/// 初期化
+	/// <summary>
+	void Initialize(Model* model, uint32_t textureHandle);
+
+	/// <summary>
+	/// 更新
+	/// <summary>
+	void Move();
+
+	/// <summary>
+	/// 更新
+	/// <summary>
+	void Update();
+
+	/// <summary>
+	/// 描画
+	/// <summary>
+	void Draw(ViewProjection& viewProjection_);
+
+	///< summary>
+	///初期化
+	///</summary>
+	void Attack();
+
+private:
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+	//モデル
+	Model* model_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+	Input* input_ = nullptr;
+	DebugText* debugText_ = nullptr;
+
+	//弾
+	std::list<std::unique_ptr<PlayerBullet>>bullets_;
+
+};
